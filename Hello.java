ac962848805fa7fd044156ae9c8825a15523cc78
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-09-29 10:46:30 +0530

    10:46

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
 	public static void main(String []args)
 	{	
-		System.out.println("Hello this is empower batch");
+		System.out.println("Hello this is empower batch...........changed now........and i will push the changes to git....");
 	}
 }
